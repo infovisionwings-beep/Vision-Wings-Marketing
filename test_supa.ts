9771a66193f96a6dc98c1ca14e4856eb6884f9a8
srijan kumar <www.srijankumar@gmail.com>
2026-07-24 08:09:30 +0530

    chore: remove unwanted debugging and test files

--- test_supa.ts
+++
@@ -1,9 +0,0 @@
-async function checkSupa() {
-  try {
-    const res = await fetch('https://supabase.com');
-    console.log("Supabase:", res.status);
-  } catch(e: any) {
-    console.error("FAILED Supabase!", e?.message || e);
-  }
-}
-checkSupa();
